4878f09989227d7430c46eb69f35820da747a417
Tim Krämer <tk@tk22.de>
2017-01-22 03:38:50 +0100

    * added image support (images under 5k will be base64-encoded into the js file

--- src/components/counter/Counter.js
+++ src/components/counter/Counter.js
@@ -15,8 +15,10 @@ class Counter extends React.Component {
     render() {
         return (
             <button className={styles.button} onClick={() => {
-                this.setState({ count: this.state.count + 1 });
+                this.setState({count: this.state.count + 1});
             }}>
+                <span style={{color: "red"}}>Test</span>
+                <img src={require("./arrow.png")} alt="arrow"/>
                 Counter: {this.state.count}
             </button>
         );
--- webpack.config.js
+++ webpack.config.js
@@ -35,6 +35,24 @@ const config = {
             {
                 test: /\.json$/,
                 loader: 'json-loader',
+            },
+            {
+                test: /.*\.(gif|png|jpe?g|svg)$/i,
+                loaders: [
+                    'url-loader?limit=5000&name=assets/img/[hash].[ext]',
+                    {
+                        loader: 'image-webpack-loader',
+                        query: {
+                            progressive: true,
+                            optimizationLevel: 7,
+                            interlaced: false,
+                            pngquant: {
+                                quality: '65-90',
+                                speed: 4
+                            }
+                        }
+                    }
+                ]
             }
         ],
     },
