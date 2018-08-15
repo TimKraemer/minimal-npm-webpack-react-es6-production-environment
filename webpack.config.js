9a3197def27c475bb94464a7673c01cc438d0859
Tim Krämer <git@tim-kraemer.de>
2018-08-15 08:43:17 +0200

    :package: Updating packages

--- webpack.config.js
+++ webpack.config.js
@@ -39,7 +39,7 @@ module.exports = (env, argv) => {
                 plugins: [autoprefixer],
               },
             },
-            // "fast-sass-loader", // FIXME: has known vulnerabilities (deactivated until fixed)
+            "fast-sass-loader",
           ],
         },
         {
